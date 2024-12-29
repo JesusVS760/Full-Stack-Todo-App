e1160f7f3fcb25077f83457c8c068d72c44f661c
JesusVA760 <tonio4340406@gmail.com>
2024-12-28 23:52:43 -0800

    Established Login Backend Connection & Database

--- client/vite-project/src/components/Login.jsx
+++ client/vite-project/src/components/Login.jsx
@@ -1,5 +1,34 @@
 import React from "react";
+import { Label } from "./ui/label";
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
+import { Link } from "react-router-dom";
 
 export const Login = () => {
-  return <div>Login Page</div>;
+  return (
+    <div className="h-screen flex justify-center items-center transform -translate-y-16">
+      <form action="" className=" flex flex-col gap-6 max-w-xl w-full px-8">
+        <div className="flex flex-col gap-2">
+          <Label>Email</Label>
+          <Input type="email" name="email" placeholder="Enter email" />
+        </div>
+        <div className="flex flex-col gap-2">
+          <Label>Password</Label>
+          <Input type="password" name="password" placeholder="Enter password" />
+        </div>
+        <Button>Login</Button>
+        <div className="text-center">
+          <span className="text-[#63657b] text-center">
+            Don't have an account?{" "}
+          </span>
+          <Link
+            to="/register"
+            className="transition ease-in-out hover:cursor-pointer hover:text-primary hover:underline"
+          >
+            Register
+          </Link>
+        </div>
+      </form>
+    </div>
+  );
 };
--- client/vite-project/src/components/Register.jsx
+++ client/vite-project/src/components/Register.jsx
@@ -1,27 +1,67 @@
-import React from "react";
+import React, { useState, useActionState, useEffect } from "react";
 import { Label } from "./ui/label";
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
-import { Link } from "react-router-dom";
-
+import { Link, Navigate, useNavigate } from "react-router-dom";
+import { register } from "../actions/userActions";
 export const Register = () => {
+  const navigate = useNavigate();
+
+  const [formData, setFormData] = useState({ email: "", password: "" });
+  const [state, formAction, isPending] = useActionState(register, {
+    sucess: null,
+    error: null,
+  });
+
+  useEffect(() => {
+    if (state.sucess) {
+      setTimeout(() => {
+        navigate("/login");
+      }, 2000);
+    }
+  }, [state.success]);
+
+  const handleChange = (event) => {
+    setFormData({ ...formData, [event.target.name]: event.target.value });
+  };
+  console.log(formData);
   return (
     <div className="h-screen flex justify-center items-center transform -translate-y-16">
-      <form action="" className=" flex flex-col gap-6 max-w-xl w-full px-8">
+      <form
+        action={formAction}
+        className=" flex flex-col gap-6 max-w-xl w-full px-8"
+      >
         <div className="flex flex-col gap-2">
           <Label>Email</Label>
-          <Input type="email" name="email" placeholder="Enter email" />
+          <Input
+            type="email"
+            name="email"
+            placeholder="Enter email"
+            value={formData.email}
+            onChange={handleChange}
+          />
         </div>
         <div className="flex flex-col gap-2">
           <Label>Password</Label>
-          <Input type="password" name="password" placeholder="Enter password" />
+          <Input
+            type="password"
+            name="password"
+            placeholder="Enter password"
+            value={formData.password}
+            onChange={handleChange}
+          />
         </div>
-        <Button>Register</Button>
+        <Button disabled={isPending}>
+          {isPending ? "Registering" : "Register"}
+        </Button>
         <div className="text-center">
           <span className="text-[#63657b] text-center">
             Already have an account?{" "}
           </span>
-          <Link to="/login" className="iconHover text-center">
+          <Link
+            to="/login"
+            className="transition ease-in-out hover:cursor-pointer hover:text-primary hover:underline"
+          >
             Login
           </Link>
         </div>
